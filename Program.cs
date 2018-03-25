25eda07048854fce92b72e0e030d25372c4d8e64
LucasRoKK <37778908+LucasRoKK@users.noreply.github.com>
2018-03-25 14:58:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------CALCULO DE UM RETÂNGULO------");
+            Console.ReadLine();
+
+            Console.Write("Digite a largura do retangulo: ");
+            Retangulo val = new Retangulo();
+            val.largura = double.Parse(Console.ReadLine());
+            Console.Write("Digite a altura do retangulo: ");
+            val.altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O valor da área é de:{0}",val.Area());
+
+            Console.WriteLine("O valor do perimetro é de:{0}", val.Perimetro());
+
+            Console.WriteLine("O valor da diagonal é de:{0}", val.Diagonal());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        
+        public double Area()
+        {
+            double a = this.altura * this.largura;
+            return a;
+        } 
+
+        public double Perimetro()
+        {
+            double b = (this.altura + this.largura) * 2;
+            return b;
+        }
+
+        public double Diagonal()
+        {
+            double c = Math.Sqrt(this.largura * this.largura + this.altura * this.altura);
+            return c;
+        }
+    }
+}
